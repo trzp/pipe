528c13b215a9bd3a5530811cf0e55173e2c43118
trzp <mrtang@nudt.edu.cn>
2018-10-27 20:46:43 +0800

    updated

--- win_named_pipe.py
+++ win_named_pipe.py
@@ -5,7 +5,7 @@
 # Contact: mrtang@nudt.edu.cn
 # Github: trzp
 # Last Modified by:   mr tang
-# Last Modified time: 2018-10-27 00:47:29
+# Last Modified time: 2018-10-27 20:46:18
 
 # named pipe Server
 # encoding: utf-8
@@ -15,21 +15,28 @@ import win32file
 import win32pipe
 import time
 import threading
+import pywintypes
+import thread
 
 
 '''
 usage:
-    WinNamedPipeServer: init -> accept (block) -> write
-    WinNamedPipeClient: init -> connect (use after server established, or an exception throw) -> read
+    WinNamedPipeServer: instantiation -> accept (block) -> write
+    WinNamedPipeClient: instantiation -> connect (use after server established, or an exception throw) -> read
 
-    asyWinNamedPipeServer: init -> put (call at any time, if clinet is not connected, the data will be discarded
-                                    use self.ok to check the state)
-    asyWinNamedPipeClient: init -> get (call at any time, if server is not establised, no data recved,)
+    hWinNamedPipeServer: instantiation -> put (call at any time, if clinet is not connected, the data will be discarded
+                                    if the connect is broken, the connection will be rebuild automatically, use self.ok 
+                                    to check the state)
+    hWinNamedPipeClient: instantiation -> get (call at any time, if server is not establised, no data recved,
+                                    this class is not suggest to use)
 '''
 
 class WinNamedPipeServer:
     def __init__(self, pipe_name):
-        self.pipe = win32pipe.CreateNamedPipe(r'\\.\pipe\%s' % pipe_name,  # 固定的命名方式
+        self.pipe_name = pipe_name
+
+    def init(self):
+        self.pipe = win32pipe.CreateNamedPipe(r'\\.\pipe\%s' % self.pipe_name,  # 固定的命名方式
                                               win32pipe.PIPE_ACCESS_DUPLEX,
                                               win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
                                               1, 65536, 65536,
@@ -41,7 +48,12 @@ class WinNamedPipeServer:
         win32pipe.ConnectNamedPipe(self.pipe, None)
 
     def close(self):
-        win32file.CloseHandle(self.pipe)
+        try:
+            win32pipe.DisconnectNamedPipe(self.pipe)
+            win32file.CloseHandle(self.pipe)
+            return 1
+        except:
+            return 0
 
     def write(self, buf):
         win32file.WriteFile(self.pipe, buf)
@@ -67,50 +79,74 @@ class WinNamedPipeClient:
     def read(self):
         return win32file.ReadFile(self.handle, 64*1024)[1]
 
-class asyWinNamedPipeServer(threading.Thread):
+class hWinNamedPipeServer:
+    '''
+    unblock connecting to client, and can rebuild connection automatically
+    '''
     def __init__(self,pipe_name):
-        self.ok = False
         self.__pipe = WinNamedPipeServer(pipe_name)
-        super(asyWinNamedPipeServer,self).__init__()
-        self.setDaemon(True)
-        self.start()
+        self.ok = False
+        self.__taccept()
 
-    def run(self):
+    def __accept(self):
+        self.__pipe.close()
+        self.__pipe.init()
         self.__pipe.accept()
         self.ok = True
 
+    def __taccept(self):    #connect to client in sub-thread so that main thread would not be block
+        thread.start_new_thread(self.__accept,())
+
     def put(self,buf):
+        ''' connection rebuild automatically
+            return value: 1-succeed 0-failure
+        '''
         if self.ok:
-            self.__pipe.write(buf)
-        return self.ok
+            try:
+                self.__pipe.write(buf)
+                return 1
+            except:
+                self.ok = False
+                self.__taccept()
+                return 0
+        else:
+            return 0
 
     def close(self):
         self.__pipe.close()
 
-class asyWinNamedPipeClient(threading.Thread):
-    def __init__(self,pipe_name,time_out=30): #timeout: seconds
-        self.ok = False
-        self.__to = time_out
-        self.__pipe = WinNamedPipeClient(pipe_name)
-        super(asyWinNamedPipeClient,self).__init__()
-        self.setDaemon(True)
-        self.start()
 
-    def run(self):
-        clk = time.clock()+self.__to
-        while time.clock()<clk:
+class hWinNamedPipeClient:
+    '''
+    automatically connect to server when server is closed
+    '''
+    def __init__(self, pipe_name):
+        self.ok = False
+        self.__pipe = hWinNamedPipeClient(pipe_name)
+        self.__tconnect()
+
+    def __connect(self):
+        while True:
             try:
                 self.__pipe.connect()
                 self.ok = True
                 break
             except:
                 pass
+            time.sleep(0.2)
 
-            time.sleep(0.1)
+    def __tconnect(self):
+        thread.start_new_thread(self.__connect,())
 
-    def get(self):
+    def read(self):
         if self.ok:
-            return 1,self.__pipe.read()
+            try:
+                buf = win32file.ReadFile(self.handle, 64*1024)[1]
+                return [1,buf]
+            except:
+                self.ok = False
+                self.__tconnect()
+                return [0,'']
         else:
-            return 0,0
+            return [0,'']
 
